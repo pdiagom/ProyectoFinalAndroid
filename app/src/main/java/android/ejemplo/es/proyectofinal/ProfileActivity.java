1176f441afc3a267b48677636f6120fa6ead64c1
Pablo <pabloalosno@gmail.com>
2025-03-02 16:44:53 +0100

    Modificaciones de estilos y arreglo logout

--- app/src/main/java/android/ejemplo/es/proyectofinal/ProfileActivity.java
+++ app/src/main/java/android/ejemplo/es/proyectofinal/ProfileActivity.java
@@ -1,7 +1,11 @@
 package android.ejemplo.es.proyectofinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -10,6 +14,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
 
         // Obtener el email desde el Intent
         String userEmail = getIntent().getStringExtra("USER_EMAIL");
@@ -23,5 +28,20 @@ public class ProfileActivity extends AppCompatActivity {
         } else {
             emailTextView.setText("No hay email disponible");
         }
+
+        // Referencia al botón de Logout
+        Button btnLogout = findViewById(R.id.btnLogout);
+
+        // Configurar el listener para cerrar sesión
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Borra el historial de actividades
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
